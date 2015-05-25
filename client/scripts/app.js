f36b10d6fe1e69a01828cc308cf9273c007a0426
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 16:25:57 -0700

    Chat Room list populating, created interface for username and message selection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
-  var postMessage = function(username,text,roomName) {
+  var userName = "Aaron Rodgers peace and love";
+  var roomName = "notre dame football";
 
+  var roomNames = [];
+
+  var postMessage = function(username,text,roomName) {
     var message = {
       'username': _.escape(username),
       'text': _.escape(text),
@@ -19,6 +23,7 @@ $(document).ready(function(){
       success: function (data) {
         console.log('chatterbox: Message sent');
         console.log(data);
+        getMessages();
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message');
@@ -27,28 +32,58 @@ $(document).ready(function(){
 
   };
 
+  var getMessages = function () {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      //data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        var results = data.results;
+        for (var i = 0; i < results.length; i++){
+          var currentObject = results[i];
+          var roomNameText = _.escape(currentObject.roomname);
 
-	$.ajax({
-	  url: 'https://api.parse.com/1/classes/chatterbox',
-	  type: 'GET',
-	  //data: JSON.stringify(message),
-	  contentType: 'application/json',
-	  success: function (data) {
-	    var results = data.results;
-      for (var i = 0; i < results.length; i++){
-        var currentObject = results[i];
+          if (roomNames.indexOf(roomNameText) === -1) {
+            roomNames.push(roomNameText);
+            addRoomName(roomNameText)
+          }
 
-        var escName = _.escape(currentObject.username);
-        var escText = _.escape(currentObject.text);
-	      $('.test').append('<div></div>').append('Username: ' + escName + '\nmessage: ' + escText);
+          var escName = _.escape(currentObject.username);
+          var escText = _.escape(currentObject.text);
+          $('.test').append('<div></div>').append('Username: ' + escName + '\nmessage: ' + escText);
+        }
+
+
+        console.log(data);
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to get message');
       }
-      console.log(data);
-	    console.log('chatterbox: Message received');
-	  },
-	  error: function (data) {
-	    console.error('chatterbox: Failed to get message');
-	  }
-	});
+    });
+  };
 
-  postMessage("valkyrie","hi","notre dame football")
+  var addRoomName = function(roomName) {
+    $('.dropdown-menu').prepend('<li id=' + roomName + ' role="presentation"><a role="menuitem" tabindex="-1" href="#">' + roomName + '</a></li>');
+  };
+
+  $('#getMessages').on('click',function() {
+    getMessages();
+  });
+
+  $('#sendMessage').on('click',function() {
+    postMessage(userName, $('#message').val(),roomName);
+    $('#message').val('')
+  });
+
+  $('#usr').on('keyup',function() {
+    userName = $(this).val();
+  });
+
+  $('.dropdown-menu li').on('click', function(){
+
+  });
+
+  getMessages();
 });
