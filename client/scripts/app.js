2022410b6bbc582965e07597015ff991256df642
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 20:24:40 -0700

    Implemented friend functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   var currentRoomName = "To All Rooms";
   var roomNames = [];
   var allMessages = [];
+  var friends = [];
 
   var postMessage = function(username,text,roomName) {
     var message = {
@@ -57,19 +58,27 @@ $(document).ready(function(){
     $('.messageBody').html('');
     for (var i = 0; i < allMessages.length; i++){
       var currentObject = allMessages[i];
+
       var roomNameText = _.escape(currentObject.roomname);
+      var escName = _.escape(currentObject.username);
+      var escText = _.escape(currentObject.text);
       //establish list of all current chat rooms
       if (roomNames.indexOf(roomNameText) === -1) {
         roomNames.push(roomNameText);
         addRoomName(roomNameText)
       }
+
       //checks to see what chat room has been selected and shows messages accordingly
       if (currentRoomName === "To All Rooms" || roomNameText === currentRoomName){
-        var escName = _.escape(currentObject.username);
-        var escText = _.escape(currentObject.text);
-        $('.messageBody').append('<div></div>').append('<div>Username: ' + escName + '\nmessage: ' + escText + '</div>').focus();
+        var element = $('.messageBody').append('<div></div>').append('<div class="' + escName.replace(/\s/g,"_") + '"><a href=# class="userLink" data-info="' + escName + '">' + escName + ': </a>' + escText + '</div><br>');
       }
     }
+
+      for (var i = 0; i < friends.length; i++) {
+        var friend = friends[i].replace(/\s/g, "_");
+        console.log($('.' + friend));
+        $('.' + friend).attr('class','friend');
+      }
   };
 
   $('#getMessages').on('click',function() {
@@ -100,6 +109,15 @@ $(document).ready(function(){
     }
     $('#menu1').html(response + ' <span class="caret"></span>');
     refreshMessageBody();
+  });
+
+  $('.messageBody').on('click','.userLink',function() {
+    var user = $(this).attr('data-info');
+    if (friends.indexOf(user) === -1) {
+      friends.push(user);
+    }
+    getMessages();
+
   });
 
   getMessages();
